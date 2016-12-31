9b301d84e202ba1bf674ce859f923cd43d91bde6
josh256 <actmago@gmail.com>
2016-12-30 22:45:08 -0300

    ready for review
    all requirements ready

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,45 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/joseam.json',
+    dataType: 'jsonp',
+    success: function(response) {
+
+      let badges = $('#badges');
+
+      $.each( response.courses.completed, function( key, obj ) {
+        //console.log( key + ": " + obj.title );
+
+        let divEle = $("<div/>", {
+          class: 'course'
+        });
+
+        let h3Ele = $("<h3/>", {
+          text: obj.title
+        });
+
+        let imgEle = $("<img/>", {
+          src: obj.badge
+        });
+
+        let aEle = $("<a/>", {
+          href: obj.url,
+          target: '_blank',
+          class: 'btn btn-primary',
+          text: 'See Course'
+        });
+
+        //div
+        divEle.append(h3Ele);
+        divEle.append(imgEle);
+        divEle.append(aEle);
+
+        //badge
+        badges.append(divEle);
+
+      });
+
+    }
+  });
 
 });
